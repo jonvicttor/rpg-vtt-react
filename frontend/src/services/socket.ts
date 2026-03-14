3b8ab5664141c50caba1774e7ee610f7eddc2f6f
Jon Victor <jonvictor.dev@gmail.com>
2026-03-14 14:49:20 -0300

    v1.7

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,15 +1,14 @@
 import { io } from 'socket.io-client';
 
-// Se estiveres no teu PC rodando local, ele usa localhost.
-// Se estiveres na Vercel (internet), ele usa o teu link do Render.
-const SOCKET_URL = window.location.hostname === 'localhost'
-  ? 'http://localhost:4000'
-  : 'https://nexus-rpg-dl3i.onrender.com';
+// Se o site detectar que está rodando no seu PC, usa localhost.
+// Se estiver na Vercel (onde seu amigo vai acessar), usa o link do Render.
+const SOCKET_URL = window.location.hostname === 'localhost' 
+    ? 'http://localhost:4000' 
+    : 'https://nexus-rpg-dl3i.onrender.com';
 
 const socket = io(SOCKET_URL, {
   transports: ['websocket'],
-  reconnection: true,
-  reconnectionAttempts: 5
+  reconnection: true
 });
 
 export default socket;
